170a3f8ecfe9704ba0d7a7dd952481f92a2fe59c
coding-mage <meenakandimadai@gmail.com>
2026-01-16 17:45:23 +0530

    Removing unwanted files

--- finance-buddy-app-backend/config/config.js
+++ finance-buddy-app-backend/config/config.js
@@ -1,6 +1,6 @@
 const config = {
     MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/bank-statements',
-    GEMINI_API_KEY: process.env.GEMINI_API_KEY || 'AIzaSyDWgPLEFJS991QbifcS-OFvYNx6zP3T9iU',
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY || 'your-default-api-key',
     GEMINI_API_URL: 'https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent'
   };
   
