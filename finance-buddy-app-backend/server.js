36fb4035ead4fc9884e3ae6cc274712dd4c953cc
coding-mage <meenakandimadai@gmail.com>
2026-06-10 15:59:38 +0200

    Update CORS configuration to allow all origins in production

--- finance-buddy-app-backend/server.js
+++ finance-buddy-app-backend/server.js
@@ -43,9 +43,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 app.use('/uploads', express.static('uploads'));
 
-app.use(cors({
-    origin: 'http://localhost:3000'
-  }));
+app.use(cors());
 
   app.use((req, res, next) => {
     console.log('CORS headers set!');
